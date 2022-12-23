37fd80fb9354d2d8a0c5c53beac1cdfb7f56ded0
Gokay <rexpo2433@gmail.com>
2022-12-23 17:01:01 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import requests
+import os
+import dotenv
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+search_input = input("Which sound you looking for?: ")
+url = f"https://freesound.org/apiv2/search/text/?query={search_input}&token={API_KEY}"
+response = requests.get(url)
+response.raise_for_status()
+data=response.json()
+nums=int(input("How many sound example you want to see? :"))
+if nums > len(data["results"]):
+    nums = len(data["results"])
+    for result in range(nums):
+        sound_id = data["results"][result]["id"]
+        sound_username = data["results"][result]["username"]
+        sound_url = f"https://freesound.org/people/{sound_username}/sounds/{sound_id}/"
+        print(f"Music URL : {sound_url}")
+        dl_url = f"https://freesound.org/apiv2/sounds/{sound_id}/download/"
+        print(f"Download URL: {url}")
+else:
+    for result in range(nums):
+        sound_id = data["results"][result]["id"]
+        sound_username = data["results"][result]["username"]
+        sound_url = f"https://freesound.org/people/{sound_username}/sounds/{sound_id}/"
+        print(f"Music URL : {sound_url}")
+        dl_url = f"https://freesound.org/apiv2/sounds/{sound_id}/download/"
+        print(f"Download URL: {url}")
---
+++ main.py
@@ -0,0 +1,41 @@
+# Search free sound website with flask and HTTP request api
+from flask import Flask, request,render_template,send_from_directory
+import urllib.request
+import requests
+import os
+import dotenv
+from dotenv import load_dotenv
+app = Flask(__name__)
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/search',methods=['POST'])
+def search():
+    query = request.form['query']
+    url = f"https://freesound.org/apiv2/search/text/?query={query}&token={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    results = data['results']
+    for result in results:
+        result['url'] = f"https://freesound.org/people/{result['username']}/sounds/{result['id']}/"
+
+        download_url = f"https://freesound.org/apiv2/sounds/{result['id']}/download/"
+
+        result['download_url'] = download_url
+        save = requests.get(download_url)
+        print(save.status_code)
+        if save.status_code == 200:
+            with open('/static/sounds/sound1.mp3', 'wb') as f:
+                f.write(response.content)
+            return send_from_directory('/static/sounds', 'sound1.mp3', mimetype='audio/mpeg')
+
+    return render_template('index.html', results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
